37e603f78f245828663e1c2440963dd079ead381
Emil D <global52600894@gmail.com>
2023-08-04 19:06:41 +0200

    initial commit

---
+++ api/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const Shoppy = require('shoppy.gg');
+const app = express();
+
+require('dotenv').config();
+
+const apiKey = process.env.SHOPPY_API_KEY;
+const port = process.env.PORT || 3000;
+
+const API = new Shoppy.API(apiKey);
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api/products', (req, res) => {
+    API.getProducts()
+        .catch(data => {
+            if (Array.isArray(data)) {
+                console.log('All products: ', data);
+                res.json(data);
+            } else {
+                console.error('Error fetching all products:', data);
+                res.status(500).json({ error: 'Failed to fetch products' });
+            }
+        });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,49 @@
+const apiUrl = "http://localhost:3000/api/products";
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) { 
+      throw new Error('Network response was not ok'); 
+    }
+    return response.json();
+  })
+  .then(data => {
+    const productContainer = document.querySelector(".product-container .grids");
+
+    data.forEach(product => {
+      const productElem = document.createElement("div");
+      productElem.className = "grid";
+      productElem.innerHTML = `
+        <a class="card">
+          <div class="wrapper">
+            <div class="image-wrapper">
+              <img src="${product.image.url}">
+            </div>
+          </div>
+          <div class="content">
+            <span class="title">${product.title}</span>
+            <span class="description">${product.description}</span>
+            <div class="card-footer">
+              <button data-shoppy-product="${product.id}" class="button style3 fit" type="submit" alt="Buy Now">Buy</button>
+              <span class="price">${product.price} <span>EUR</span></span> <!-- Assuming 'price' property holds the price -->
+            </div>
+          </div>
+        </a>
+      `;
+      productContainer.appendChild(productElem);
+    });
+
+    const script = document.createElement('script');
+    script.src = 'https://shoppy.gg/api/embed.js';
+    document.body.appendChild(script);
+  })
+  .catch(error => {
+    console.error('Error: ', error);
+    const productContainer = document.querySelector(".product-container");
+    productContainer.innerHTML = '';
+    const messageContainer = document.createElement('div');
+    //center the msg
+    messageContainer.style.textAlign = 'center';
+    messageContainer.innerHTML = '<h2>Server is under maintenance.</h2>';
+    productContainer.appendChild(messageContainer);
+  });
